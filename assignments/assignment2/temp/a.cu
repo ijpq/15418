e45daabcd4d6c1c31bff306054ec148b3701dcb8
ijpq <509634578tk@gmail.com>
2022-04-09 23:47:57 +0800

    separate compile test

---
+++ assignments/assignment2/temp/a.cu
@@ -0,0 +1,19 @@
+//---------- a.cu ----------
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <stdio.h>
+#include "b.h"
+#include "a.h"
+
+
+__global__ void foo (void) {
+  printf("calling from kernel foo: %d\n", threadIdx.x);
+  bar();
+}
+
+void warperFoo() {
+    printf("calling from warperFoo\n");
+    dim3 gdim(1,1,1);
+    dim3 bdim(4,4,4);
+    foo<<<gdim, bdim>>>();
+}
---
+++ assignments/assignment2/temp/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "a.h"
+
+using namespace std;
+
+
+int main() {
+    warperFoo();   
+}
